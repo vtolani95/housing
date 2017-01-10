16f11167becb2d931e38fecaf466f3199eb44cff
Varun Tolani <vatolani@gmail.com>
2017-01-10 11:49:17 +0530

    updates

--- model2.py
+++ model2.py
@@ -5,7 +5,7 @@ from sklearn import linear_model
 from sklearn import preprocessing
 from sklearn.utils import shuffle
 import sklearn.metrics as metrics
-import util
+import util2 as util
 
 def standardize_data(train, test):
   scaler = preprocessing.StandardScaler().fit(train)
@@ -19,8 +19,9 @@ def predict(model, X):
   return np.exp(model.predict(X))
 
 def score(y, pred, name):
-  print(name + " RMS: {0}".format(metrics.mean_squared_error(y, pred)))
-  print(name + " R2: {0}".format(metrics.r2_score(y, pred)))
+  def rmsle(y, pred):
+    return np.sqrt(np.average((np.log(pred+1) - np.log(y+1))**2))
+  print(name + " RMSLE: %.6f R2: %.6f" % (rmsle(y, pred), metrics.r2_score(y, pred)))
 
 def write_csv(filename, id, pred):
   c = csv.writer(open(filename, "wt"))
@@ -28,17 +29,20 @@ def write_csv(filename, id, pred):
   for i in range(len(pred)):
     c.writerow((id[i], pred[i]))
 
+np.random.seed(35)
 Xtrain, ytrain = util.load_data('./data/train.csv')
 Xtest, test_ids = util.load_data('./data/test.csv', False)
+
+Xtrain, ytrain = shuffle(Xtrain, ytrain.T)
+Xtrain, ytrain, Xvalid, yvalid = split_data(Xtrain, ytrain)
 # Xtrain, Xtest = standardize_data(Xtrain, Xtest)
-# Xtrain, ytrain = shuffle(Xtrain, ytrain.T)
-# Xtrain, ytrain, Xvalid, yvalid = split_data(Xtrain, ytrain)
+
 reg = linear_model.LinearRegression()
 reg.fit(Xtrain, ytrain)
 train_pred = predict(reg, Xtrain)
-# valid_pred = predict(reg, Xvalid)
+valid_pred = predict(reg, Xvalid)
 score(np.exp(ytrain), train_pred, 'Training')
-# score(np.exp(yvalid), valid_pred, 'Validation')
+score(np.exp(yvalid), valid_pred, 'Validation')
 
 ytest = predict(reg, Xtest)
 write_csv('model2.csv', test_ids, ytest)
